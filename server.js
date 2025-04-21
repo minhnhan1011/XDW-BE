62fbc7a77b906ca4d3aa104bf4cd115f61a813f1
Minh Nhan <160696330+minhnhan1011@users.noreply.github.com>
2025-04-22 00:04:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: ["https://xdw-fe.vercel.app/"],
+    origin: ["https://xdw-fe.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
 }));
